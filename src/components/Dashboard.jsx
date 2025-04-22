37167a2caa203c15732eed843bb1ffccfa4227df
Roopak Mahadane <roopakmahadane@192.168.1.13>
2025-04-23 01:08:33 +0530

    V1 completed

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,59 +1,237 @@
 import Header from "./Header";
 import { useState, useEffect } from 'react'
-import {ethers} from 'ethers';
+import {ethers, formatEther} from 'ethers';
 import Escrow from '../../artifacts/contracts/Escrow.sol/Escrow.json';
 
 export default function Dashboard(){
-
     const [createdByUser, setCreatedByUser] = useState([]);
     const [userIsArbiter, setUserIsArbiter] = useState([]);
     const [userIsDriver, setUserIsDriver] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [isDisabled, setIsDisabled] = useState(new Set());
+    const [accountChanged, setAccountChanged] = useState(false);
+
+
     useEffect(() => {
-
-        const fetchEscrowEvents = async() => {
-            const provider = new ethers.BrowserProvider(window.ethereum);
-        const signer = await provider.getSigner();
-        const userAddress = await signer.getAddress();
-        const contract = await new ethers.Contract(
-            "0xFa1994bc4161F0Ff135691EE40Cda9f942A0f570",
-                Escrow.abi,
-                signer
-        )
-        const deploymentBlock = 8163756
-
-        const newEscrowEvents = await contract.queryFilter(contract.filters.NewEscrow(), deploymentBlock, 'latest');
-        const createdByUser = await contract.queryFilter(contract.filters.NewEscrow(null, userAddress ), deploymentBlock, 'latest');
-        const userIsArbiter = await contract.queryFilter(contract.filters.NewEscrow(null, null, userAddress ), deploymentBlock, 'latest');
-
-        const userIsDriver = newEscrowEvents.filter(event => 
-           // console.log("inside filter", event.args.driver, userAddress);
-             event.args.driver == userAddress );
-
-      
-        setCreatedByUser(createdByUser);
-        setUserIsArbiter(userIsArbiter);
-        setUserIsDriver(userIsDriver);
-        console.log(userIsDriver, "user Driver")
-        console.log("driver", newEscrowEvents[0].args.driver )
-        console.log("userAdd", userAddress);
-        console.log("all events", newEscrowEvents);
-        console.log(createdByUser, "Created by user");
-        console.log(userIsArbiter, "Arbiter user");
-        console.log(userIsDriver, "driver user");
+        const handleAccountChange = (accounts) => {
+            console.log("MetaMask account changed:", accounts[0]);
+            setAccountChanged(prev => !prev);   
         }
+        
+        const fetchEscrowEvents = async() => {
+            try {
+                
+                if(window.ethereum){
+                    window.ethereum.on('accountsChanged', handleAccountChange )
+                }
+
+                setLoading(true);
+                const provider = new ethers.BrowserProvider(window.ethereum);
+                const signer = await provider.getSigner();
+                const userAddress = await signer.getAddress();
+                const contract = await new ethers.Contract(
+                    "0xFa1994bc4161F0Ff135691EE40Cda9f942A0f570",
+                    Escrow.abi,
+                    signer
+                );
+                const deploymentBlock = 8163756;
+
+                const newEscrowEvents = await contract.queryFilter(contract.filters.NewEscrow(), deploymentBlock, 'latest');
+                const userEscrow = await contract.queryFilter(contract.filters.NewEscrow(null, userAddress), deploymentBlock, 'latest');
+                
+                let byUser = [];
+                for(let i = 0; i < userEscrow.length; i++) {
+                    let escrow = await contract.escrows(userEscrow[i].args.id);
+                    const resultArray = [...escrow];
+                    byUser.push(resultArray);
+                }
+                
+                console.log("Fetched escrows:", byUser);
+                setCreatedByUser(byUser);
+
+                const arbiterEvents = await contract.queryFilter(contract.filters.NewEscrow(null, null, userAddress), deploymentBlock, 'latest');
+                let asArbiter = [];
+                for(let i = 0; i < arbiterEvents.length; i++) {
+                    let escrow = await contract.escrows(arbiterEvents[i].args.id);
+                    const resultArray = [...escrow];
+                    resultArray.push(arbiterEvents[i].args.id);
+                    asArbiter.push(resultArray);
+                }
+                console.log(" as arbiter", asArbiter)
+                setUserIsArbiter(asArbiter);
+
+                const driverEvents = newEscrowEvents.filter(event => event.args.driver === userAddress);
+                let asDriver = [];
+                for(let i = 0; i < driverEvents.length; i++) {
+                    let escrow = await contract.escrows(driverEvents[i].args.id);
+                    const resultArray = [...escrow];
+                    resultArray.push(driverEvents[i].args.id);
+                    asDriver.push(resultArray);
+                }
+                console.log("asDriver",asDriver)
+                setUserIsDriver(asDriver);
+                
+            } catch (error) {
+                console.error("Error fetching escrow events:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
         fetchEscrowEvents();
 
+        return () => {
+            if(window.ethereum){
+                window.ethereum.removeListener('accountsChanged', handleAccountChange);
+            }
+        }
+    }, [accountChanged]);
 
-    },[])
+    const getStatusText = (x) => {
+        if (!x[4]) return "Yet to be picked";
+        if (x[4] && !x[5]) return "On the way";
+        if (x[4] && x[5] && !x[6]) return "Delivered";
+        if (x[4] && x[5] && x[6]) return "Order completed";
+    }
+    const getButtonText = (x) => {
+        if (!x[4]) return "Mark as Picked up";
+        if (x[4] && !x[5]) return "Mark as Delivered";
+        if (x[4] && x[5] && !x[6]) return "Release the amount";
+        if (x[4] && x[5] && x[6])  return "Order completed";
+        
+    };
 
+   
 
+    const handleStatusChange = async(id) => {
+        setIsDisabled(prev => new Set(prev).add(id));
+        try {
+            
+            const provider = new ethers.BrowserProvider(window.ethereum);
+                const signer = await provider.getSigner();
+                const contract = await new ethers.Contract(
+                    "0xFa1994bc4161F0Ff135691EE40Cda9f942A0f570",
+                    Escrow.abi,
+                    signer
+                );
+                const escrow = await contract.escrows(id);
+        if(escrow.pickedUp == false){
+            const markedPickedUp = await contract.markedPickedUp(id);
+            await markedPickedUp.wait();
+        }
+        if(escrow.pickedUp && !escrow.delivered){
+            const markDelivered = await contract.markDelivered(id);
+            await markDelivered.wait();
+        }
+        if(escrow.pickedUp && escrow.delivered && !escrow.released){
+            const markReleased = await contract.releaseToDriver(id);
+            await markReleased.wait();
+        }
+
+        const updatedEscrow = await contract.escrows(id);
+        const updatedArray = [...updatedEscrow];
+        updatedArray.push(id);
+        setUserIsDriver(prev =>
+            prev.map(item => item[7] === id ? updatedArray : item)
+        );
+        setIsDisabled(prev => {
+            const newSet = new Set(prev);
+            newSet.delete(id);
+            return newSet;
+        });
+     } catch(err){
+
+            console.log("Transaction Cancel",err)
+            setIsDisabled(prev => {
+                const newSet = new Set(prev);
+                newSet.delete(id);
+                return newSet;
+            });
+        }
+    }
+    
+    if (loading) {
+        return (
+            <div className="p-4 mt-20 max-w-sm w-full mx-auto">
+            <div className="animate-pulse flex space-x-4">
+              <div className="rounded-full bg-slate-200 h-10 w-10"></div>
+              <div className="flex-1 space-y-6 py-1">
+                <div className="h-2 bg-slate-200 rounded"></div>
+                <div className="space-y-3">
+                  <div className="grid grid-cols-3 gap-4">
+                    <div className="h-2 bg-slate-200 rounded col-span-2"></div>
+                    <div className="h-2 bg-slate-200 rounded col-span-1"></div>
+                  </div>
+                  <div className="h-2 bg-slate-200 rounded"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+    }
 
     return(
         <div>
-            < Header />
-            {/* {createdByUser.map(x => {
-                <h3>{x.id}</h3>
-            })} */}
+            <Header />
+            <div className="w-screen h-screen m-0 p-0 md:flex md:flex-row md:justify-center md:align-middle">
+                <div className="md:w-1/3 w-screen">
+                    <h1 className="mt-20 text-2xl text-center font-mono font-light">Created by you</h1>
+                    {createdByUser.length > 0 ? (
+                        createdByUser.map((x, index) => (
+                            <div key={index} className="bg-white rounded-2xl shadow-lg p-6 transform transition m-10 px-10 duration-500 ">
+                                <p className="text-sm break-words">Driver: {x[1]}</p>
+                                <p className="text-sm break-words">Recipient: {x[2]}</p>
+                                <p className="text-sm break-words">Amount: {ethers.formatEther(x[3])} ETH</p>
+                                <p className="text-sm break-words">Status: {getStatusText(x)}</p>
+                            </div>
+                        ))
+                    ) : (
+                        <div className="text-center mt-4">No escrows created by you</div>
+                    )}
+                </div>
+
+                <div className="md:w-1/3 w-screen">
+                    <h1 className="mt-20 text-2xl text-center font-mono font-light">As Drivers</h1>
+                    {userIsDriver.length > 0 ? (
+                        userIsDriver.map((x) => (
+
+                            <div className="bg-white rounded-2xl shadow-lg p-6 transform transition m-10 px-10 duration-500 ">
+                                <p className="text-sm break-words">Customer: {x[0]}</p>
+                                <p className="text-sm break-words">Recipient: {x[2]}</p>
+                                <p className="text-sm break-words">Amount: {ethers.formatEther(x[3])} ETH</p>
+                                <p className="text-sm break-words">Status: {getStatusText(x)}</p>
+                                { (x[5]) ? null : <button  class={`${isDisabled.has(x[7]) ? "text-white bg-blue-400 cursor-not-allowed opacity-60 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center" : "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"}`} 
+                                onClick={() => handleStatusChange(x[7])}>
+                                    {getButtonText(x)}
+                                </button>}
+                                
+                            </div>
+                        ))
+                    ) : (
+                        <div className="text-center mt-4">No escrows assigned to you as driver</div>
+                    )}
+                </div>
+
+                <div className="md:w-1/3 w-screen">
+                    <h1 className="mt-20 text-2xl text-center font-mono font-light">As arbiters</h1>
+                    {userIsArbiter.length > 0 ? (
+                        userIsArbiter.map((x) => (
+                            <div className="bg-white rounded-2xl shadow-lg p-6 transform transition m-10 px-10 duration-500 ">
+                                <p className="text-sm break-words">Customer: {x[0]}</p>
+                                <p className="text-sm break-words">Driver: {x[1]}</p>
+                                <p className="text-sm break-words">Amount: {ethers.formatEther(x[3])} ETH</p>
+                                <p className="text-sm break-words">Status: {getStatusText(x)}</p>
+                                { (x[5]) && !x[6]? <button  class={`${isDisabled.has(x[7]) ? "text-white bg-blue-400 cursor-not-allowed opacity-60 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center" : "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"}`} 
+                                onClick={() => handleStatusChange(x[7])}>
+                                    {getButtonText(x)}
+                                </button>: null}
+                            </div>
+                        ))
+                    ) : (
+                        <div className="text-center mt-4">No escrows assigned to you as arbiter</div>
+                    )}
+                </div>
+            </div>
         </div>
-    )
+    );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
 import '../App.css'
 import ConnectWallet from './ConnectWalletButton'
+import { Link } from 'react-router-dom';
 
 export default function Header() {
     return(
         <div className="flex items-center justify-between py-5 bg-white  w-screen bg-amber-300 px-10">
+            <div className='flex'>
+            <Link  to="/">
             <h1 className="text-4xl font-mono font-bold text-gray-800 ">Escrow Connect</h1>
+            </Link>
+            <Link to="/createEscrow" className="hover:underline">
+            <h3 className="p-2.5 ml-6 font-mono text-gray-700 font-bold">Create Escrow</h3>
+            </Link>
+            <Link to="/dashboard" className="hover:underline">
+            <h3 className="p-2.5 ml-1 font-mono text-gray-700 font-bold">Dashboard</h3>
+            </Link>
+           
+            </div>
+            
+           
             <ConnectWallet/>
         </div>
     )
