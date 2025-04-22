02fcba71de7612745fb7120434d5acd4e293fa95
Roopak Mahadane <roopakmahadane@192.168.1.13>
2025-04-23 01:30:29 +0530

    minor changes

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,7 @@ import Header from "./Header";
 import { useState, useEffect } from 'react'
 import {ethers, formatEther} from 'ethers';
 import Escrow from '../../artifacts/contracts/Escrow.sol/Escrow.json';
+import Loader from "./Loader";
 
 export default function Dashboard(){
     const [createdByUser, setCreatedByUser] = useState([]);
@@ -152,20 +153,14 @@ export default function Dashboard(){
     
     if (loading) {
         return (
-            <div className="p-4 mt-20 max-w-sm w-full mx-auto">
-            <div className="animate-pulse flex space-x-4">
-              <div className="rounded-full bg-slate-200 h-10 w-10"></div>
-              <div className="flex-1 space-y-6 py-1">
-                <div className="h-2 bg-slate-200 rounded"></div>
-                <div className="space-y-3">
-                  <div className="grid grid-cols-3 gap-4">
-                    <div className="h-2 bg-slate-200 rounded col-span-2"></div>
-                    <div className="h-2 bg-slate-200 rounded col-span-1"></div>
-                  </div>
-                  <div className="h-2 bg-slate-200 rounded"></div>
+            <div>
+                <Header />
+                <div className="flex">
+                    <Loader />
+                    <Loader />
+                    <Loader />
                 </div>
-              </div>
-            </div>
+        
           </div>
         );
     }
